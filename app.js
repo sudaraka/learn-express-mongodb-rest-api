8b531cb342bc6aafe33ab9515b6c2ae123528490
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2017-08-27 16:41:26 +0530

    Add "404 Not Found" fallback route

--- app.js
+++ app.js
@@ -8,5 +8,15 @@ const
 // Middleware
 app.use(logger('dev'))
 
+// Catch 404 Errors and forward to error handlers
+app.use((req, res, next) => {
+  const
+    err = new Error('Not Found')
+
+  err.status = 404
+
+  next(err)
+})
+
 // Start server
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
