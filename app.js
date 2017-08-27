eaf1d94578aa38267a364b5fde6bbc8399429d9b
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2017-08-27 16:30:31 +0530

    Log HTTP requests to console via morgan

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const
   express = require('express'),
+  logger = require('morgan'),
 
   app = express(),
   port = app.get('port') || 5000
+
+// Middleware
+app.use(logger('dev'))
 
 // Start server
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
