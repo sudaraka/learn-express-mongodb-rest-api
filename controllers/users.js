373fbc24c5d00a7705ba5208553cb02a6bcb8d8b
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2017-09-04 22:22:12 +0530

    Switch to use Promises in mongoose calls

--- controllers/users.js
+++ controllers/users.js
@@ -3,29 +3,31 @@ const
 
 module.exports = {
   'index': (req, res, next) => {
-    User.find({}, (err, users) => {
-      if(err) {
+    User
+      .find({})
+      .then(users => {
+        res
+          .status(200)
+          .json(users)
+      })
+      .catch(err => {
         next(err)
-      }
-
-      res
-        .status(200)
-        .json(users)
-    })
+      })
   },
 
   'newUser': (req, res, next) => {
     const
       newUser = new User(req.body)
 
-    newUser.save((err, user) => {
-      if(err) {
+    newUser
+      .save()
+      .then(user => {
+        res
+          .status(201)
+          .json(user)
+      })
+      .catch(err => {
         next(err)
-      }
-
-      res
-        .status(201)
-        .json(user)
-    })
+      })
   }
 }
